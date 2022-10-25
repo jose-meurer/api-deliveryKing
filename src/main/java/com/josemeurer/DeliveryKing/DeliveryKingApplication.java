ff621def5c85f5a6dd76c176bc44612c4be68f90
josemeurer <josewmeurer@gmail.com>
2022-10-25 14:04:55 -0300

    Spring project

---
+++ src/main/java/com/josemeurer/DeliveryKing/DeliveryKingApplication.java
@@ -0,0 +1,13 @@
+package com.josemeurer.DeliveryKing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryKingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josemeurer/DeliveryKing/DeliveryKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josemeurer.DeliveryKing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryKingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
