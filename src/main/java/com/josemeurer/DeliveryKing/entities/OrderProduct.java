0cd683fa4374b17bf35237fe92e1f21ea3c12bda
josemeurer <josewmeurer@gmail.com>
2022-10-25 22:42:45 -0300

    FEAT OrderProduct

---
+++ src/main/java/com/josemeurer/DeliveryKing/entities/OrderProduct.java
@@ -0,0 +1,80 @@
+package com.josemeurer.DeliveryKing.entities;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_orderProduct")
+public class OrderProduct implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+
+    @Column(columnDefinition = "TEXT")
+    private String observation;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public OrderProduct() {
+    }
+
+    public OrderProduct(Long id, Integer quantity, String observation, Product product) {
+        this.id = id;
+        this.quantity = quantity;
+        this.observation = observation;
+        this.product = product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderProduct that = (OrderProduct) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
