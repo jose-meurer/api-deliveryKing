6cb09bac6d0aba229d9c70213ce893399e4afbee
josemeurer <josewmeurer@gmail.com>
2022-10-26 15:48:39 -0300

    FEAT Role

--- src/main/java/com/josemeurer/DeliveryKing/entities/User.java
+++ src/main/java/com/josemeurer/DeliveryKing/entities/User.java
@@ -34,6 +34,13 @@ public class User implements Serializable {
 
     //criar lista enderecos
 
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
     public User() {
     }
 
