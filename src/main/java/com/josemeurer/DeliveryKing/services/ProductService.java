cd5096b1de05781c4c4eeffdec7e63dfe587ed8c
josemeurer <josewmeurer@gmail.com>
2022-10-27 21:06:48 -0300

    FEAT Product insert

--- src/main/java/com/josemeurer/DeliveryKing/services/ProductService.java
+++ src/main/java/com/josemeurer/DeliveryKing/services/ProductService.java
@@ -3,6 +3,7 @@ package com.josemeurer.DeliveryKing.services;
 import com.josemeurer.DeliveryKing.dtos.ProductDTO;
 import com.josemeurer.DeliveryKing.dtos.ProductMinDTO;
 import com.josemeurer.DeliveryKing.entities.Product;
+import com.josemeurer.DeliveryKing.repositories.CategoryRepository;
 import com.josemeurer.DeliveryKing.repositories.ProductRepository;
 import com.josemeurer.DeliveryKing.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+
     @Transactional(readOnly = true)
     public Page<ProductMinDTO> findAllPaged(Pageable pageable) {
         Page<Product> list = productRepository.findAll(pageable);
@@ -30,5 +35,23 @@ public class ProductService {
         Optional<Product> obj = productRepository.findById(id);
         Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
         return new ProductDTO(entity, entity.getCategories());
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        copyDtoToEntity(dto, entity);
+        entity = productRepository.save(entity);
+        return new ProductDTO(entity, entity.getCategories());
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setPrice(dto.getPrice());
+        entity.setDescription(dto.getDescription());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity.getCategories().clear();
+        dto.getCategories().forEach(x -> entity.getCategories().add(categoryRepository.getReferenceById(x.getId())));
     }
 }
