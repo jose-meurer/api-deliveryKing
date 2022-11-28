56320b7e73f76aec214aff8aa7f91a942e9daea2
josemeurer <josewmeurer@gmail.com>
2022-11-28 16:14:49 -0300

    FEAT Users myProfile, delete myProfile

--- src/main/java/com/josemeurer/DeliveryKing/controllers/UserController.java
+++ src/main/java/com/josemeurer/DeliveryKing/controllers/UserController.java
@@ -1,8 +1,12 @@
 package com.josemeurer.DeliveryKing.controllers;
 
+import com.josemeurer.DeliveryKing.dtos.UserMaxDTO;
 import com.josemeurer.DeliveryKing.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +18,17 @@ public class UserController {
     private UserService userService;
 
     //User
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @GetMapping(path = "/myprofile")
+    public ResponseEntity<UserMaxDTO> myProfile() {
+        UserMaxDTO dto = userService.findMyProfile();
+        return ResponseEntity.ok(dto);
+    }
 
-    public ResponseEntity<>
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @DeleteMapping(path = "/myprofile")
+    public ResponseEntity<Void> deleteMyProfile() {
+        userService.deleteMyProfile();
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/josemeurer/DeliveryKing/services/UserService.java
+++ src/main/java/com/josemeurer/DeliveryKing/services/UserService.java
@@ -1,7 +1,44 @@
 package com.josemeurer.DeliveryKing.services;
 
+import com.josemeurer.DeliveryKing.dtos.UserMaxDTO;
+import com.josemeurer.DeliveryKing.entities.User;
+import com.josemeurer.DeliveryKing.repositories.UserRepository;
+import com.josemeurer.DeliveryKing.services.exceptions.DatabaseException;
+import com.josemeurer.DeliveryKing.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public UserMaxDTO findMyProfile() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User entity = userRepository.findMyProfile(email);
+        return new UserMaxDTO(entity, entity.getPhones(), entity.getAddresses(), entity.getRoles());
+    }
+
+    public void deleteMyProfile() {
+
+        try {
+            String email = SecurityContextHolder.getContext().getAuthentication().getName();
+            User entity = userRepository.findByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+            userRepository.deleteById(entity.getId());
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("User not found");
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
 }
